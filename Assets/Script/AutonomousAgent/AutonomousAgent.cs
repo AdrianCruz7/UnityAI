182883fa870ec9f236b5f925d9c59abc0e36c265
acruz1 <acruz@student.neumont.edu>
2023-01-19 10:58:15 -0700

    Nav Agent

--- Assets/Script/AutonomousAgent/AutonomousAgent.cs
+++ Assets/Script/AutonomousAgent/AutonomousAgent.cs
@@ -5,30 +5,18 @@ using UnityEngine;
 public class AutonomousAgent : Agent
 {
     public Perception flockPerception;
-
-    [Range(0, 3)] public float seekWeight;
-    [Range(0, 3)] public float fleeWeight;
-
-    [Range(0, 3)] public float cohesionWeight;
-    [Range(0, 3)] public float separationWeight;
-    [Range(0, 3)] public float alignmentWeight;
-     
-    [Range(0, 10)] public float separationRadius;
-
-    public float wanderDistance = 1;
-    public float wanderRadius = 3;
-    public float wanderDisplacement = 5; 
+    public ObstacleAvoidance obstacleAvoidance;
+    public AutonomousAgentData data;
   
     public float wanderAngle { get; set; } = 0; 
     
     void Update()
     {
-
         var gameObjects = perception.GetGameObjects();
         if (gameObjects.Length > 0)
         {
-            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * seekWeight);
-            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * fleeWeight);
+            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * data.seekWeight);
+            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * data.fleeWeight);
         }
         
         gameObjects = flockPerception.GetGameObjects();
@@ -39,18 +27,27 @@ public class AutonomousAgent : Agent
                 Debug.DrawLine(transform.position, gameObject.transform.position);
             }
             
-            movement.ApplyForce(Steering.Cohesion(this, gameObjects) * cohesionWeight);
-            movement.ApplyForce(Steering.Separation(this, gameObjects, separationRadius) * separationWeight);
-            movement.ApplyForce(Steering.Alignment(this, gameObjects) * alignmentWeight);
+            movement.ApplyForce(Steering.Cohesion(this, gameObjects) * data.cohesionWeight);
+            movement.ApplyForce(Steering.Separation(this, gameObjects, data.separationRadius) * data.separationWeight);
+            movement.ApplyForce(Steering.Alignment(this, gameObjects) * data.alignmentWeight);
         }
 
+        // obstacle avoidance 
+        if (obstacleAvoidance.IsObstacleInFront())
+        {
+            Vector3 direction = obstacleAvoidance.GetOpenDirection();
+            movement.ApplyForce(Steering.CalculateSteering(this, direction) * data.obstacleWeight);
+        }
 
-
+        //wander
         if (movement.acceleration.sqrMagnitude <= movement.maxForce * 0.1f)
         {
             movement.ApplyForce(Steering.Wander(this));
         }
 
-        transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
+        Vector3 position = transform.position;
+        position = Utilities.Wrap(position, new Vector3(-20, -20, -20), new Vector3(20, 20, 20));
+        position.y = 0;
+        transform.position = position;
     }
 }
